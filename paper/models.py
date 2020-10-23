d4509b13c53879bfbda83e7e6a194bb2071b21d2
y.kakizaki <y.kakizaki@rakumo.com>
2020-10-23 14:10:21 +0900

    inital commit

---
+++ paper/models.py
@@ -0,0 +1,31 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+class Category(models.Model):
+    category_name=models.CharField(max_length=50)
+    # readed=models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.category_name
+    
+
+class Post(models.Model):
+    name=models.ForeignKey(settings.AUTH_USER_MODEL,
+    on_delete=models.CASCADE)
+    title=models.CharField(max_length=200)
+    URL=models.CharField(max_length=200)
+    summary=models.TextField()
+    category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
